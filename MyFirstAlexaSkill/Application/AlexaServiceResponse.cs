116bda84ae8da4d08862870bdaec5927f9f44439
Josh Rolstad <jorolsta@microsoft.com>
2017-06-12 14:01:37 -0700

    Defining request / response base on documentation

--- MyFirstAlexaSkill/Application/AlexaServiceResponse.cs
+++ MyFirstAlexaSkill/Application/AlexaServiceResponse.cs
@@ -6,30 +6,6 @@ using System.Threading.Tasks;
 
 namespace MyFirstAlexaSkill.Application
 {
-    public class OutputSpeech
-    {
-        public string type { get; set; }
-        public string text { get; set; }
-    }
-
-    public class Card
-    {
-        public string content { get; set; }
-        public string title { get; set; }
-        public string type { get; set; }
-    }
-
-    public class Response
-    {
-        public OutputSpeech outputSpeech { get; set; }
-        public Card card { get; set; }
-        public bool shouldEndSession { get; set; }
-    }
-
-    public class SessionAttributes
-    {
-    }
-
     public class AlexaServiceResponse
     {
         public static AlexaServiceResponse CreateOutputSpeechResponse(string intent, string outputSpeech)
@@ -37,7 +13,7 @@ namespace MyFirstAlexaSkill.Application
             var response = new AlexaServiceResponse
             {
                 version = "1.1",
-                sessionAttributes = new SessionAttributes(),
+                sessionAttributes = new Dictionary<string, string>(),
                 response = new Response
                 {
                     outputSpeech = new OutputSpeech
@@ -59,6 +35,65 @@ namespace MyFirstAlexaSkill.Application
         }
         public string version { get; set; }
         public Response response { get; set; }
-        public SessionAttributes sessionAttributes { get; set; }
+        public Dictionary<string, string> sessionAttributes { get; set; }
     }
+
+    public class Response
+    {
+        public OutputSpeech outputSpeech { get; set; }
+        public Card card { get; set; }
+        public Reprompt reprompt { get; set; }
+        public List<Directive> directives { get; set; }
+        public bool shouldEndSession { get; set; }
+    }
+
+    public class OutputSpeech
+    {
+        public string type { get; set; }
+        public string text { get; set; }
+        public string ssml { get; set; }
+    }
+
+    public class Card
+    {
+        public string content { get; set; }
+        public string title { get; set; }
+        public string type { get; set; }
+        public string text { get; set; }
+        public AlexaImage image { get; set; }
+    }
+
+    public class AlexaImage
+    {
+        public string smallImageUrl { get; set; }
+        public string largeImageUrl { get; set; }
+    }
+
+    public class Reprompt
+    {
+        public OutputSpeech outputSpeech { get; set; }
+    }
+
+    public class Directive
+    {
+        public string type { get; set; }
+        public string playBehavior { get; set; }
+        public AudioItem audioItem { get; set; }
+    }
+
+    public class AudioItem
+    {
+        public AlexaStream stream { get; set; }
+    }
+
+    public class AlexaStream
+    {
+        public string token { get; set; }
+        public string url { get; set; }
+        public int offsetInMilliseconds { get; set; }
+    }
+
+
+
+
 }
